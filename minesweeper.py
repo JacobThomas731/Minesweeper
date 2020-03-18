7721046890aa6b3dd1df213d03d872579b0cf526
wannabe-developer <48841003+JacobThomas731@users.noreply.github.com>
2020-03-18 16:02:31 +0530

    Update minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -6,8 +6,9 @@
 # discussing with me about this project.
 import pygame, sys, random, os
 from queue import Queue
+
 if sys.platform in ["WIN32", "WIN64"]:
-        os.environ["SDL_VIDEO_CENTERED"] = "1"
+    os.environ["SDL_VIDEO_CENTERED"] = "0"
 pygame.init()
 flag = pygame.image.load('minesweeper_images/redflag1.png')
 grayTile = pygame.image.load('minesweeper_images/graytile.png')
@@ -30,47 +31,47 @@ youWin = pygame.image.load('minesweeper_images/youWin.png')
 FPSCLOCK = pygame.time.Clock()
 FPS = 40
 TILESIZE = 27
-GAP = 4 # gap btw 2 tiles
+GAP = 4  # gap btw 2 tiles
 PADDING = 8
+
 
 def main():
     global GRID, BOMBS, SIZE, BOMB, WIN, SCREEN_WIDTH, SCREEN_HEIGHT, TOTAL_FLAGS
     (SCREEN_WIDTH, SCREEN_HEIGHT) = (menuSelection[0][0], menuSelection[0][1])
-    SIZE = (menuSelection[1][0], menuSelection[1][1]) # GRID SIZE
-    BOMB = menuSelection[2] # no of BOMBS in the game
+    SIZE = (menuSelection[1][0], menuSelection[1][1])  # GRID SIZE
+    BOMB = menuSelection[2]  # no of BOMBS in the game
     TOTAL_FLAGS = BOMB
     WIN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE)
     pygame.display.set_caption('Minesweeper')
     WIN.fill((146, 146, 146))
     running = True
-    clicked = False
-    noClick = True # to register the first click
+    noClick = True  # to register the first click
     mouseBut = None
     startTime = 0
     resetButHovered = None
     GRID = initalize_tiles()
-    while running == True:
+    while running:
         clicked = False
         for event in pygame.event.get():
-            if event.type == pygame.QUIT or\
+            if event.type == pygame.QUIT or \
                     (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 pygame.quit()
                 sys.exit()
             elif event.type == pygame.MOUSEBUTTONDOWN and (pygame.mouse.get_pressed()[0] == 1 or
-                                            pygame.mouse.get_pressed()[2] == 1):
+                                                           pygame.mouse.get_pressed()[2] == 1):
                 clicked = True
                 mouseBut = 0 if pygame.mouse.get_pressed()[0] == 1 else 1
-                if resetButHovered != None:
+                if resetButHovered is not None:
                     main()
-                if mouse_hover() and noClick: # will be called only at the first click on a tile
+                if mouse_hover() and noClick:  # will be called only at the first click on a tile
                     if mouseBut == 0:
                         startTime = pygame.time.get_ticks()
                         BOMBS = bomb_gen(pygame.mouse.get_pos())
                         noClick = False
-        resetButHovered = score_space(startTime) # manages the score space-> time, flags, reset button etc
+        resetButHovered = score_space(startTime)  # manages the score space-> time, flags, reset button etc
         if clicked:
             bombClicked = tile_clicked(mouseBut)
-        allTilesOpen = display_tiles() # opens up all the neighbouring tiles, return True if won
+        allTilesOpen = display_tiles()  # opens up all the neighbouring tiles, return True if won
         if mouse_hover() and clicked:
             if bombClicked == 'Bomb':
                 running = False
@@ -84,14 +85,15 @@ def main():
         FPSCLOCK.tick(30)
         pygame.time.wait(30)
 
+
 def score_space(startTime):
     """
     This fun() was added later and is used to manage all the working in the score space.
     Takes in as parameter the time pygame.init() was called to manage the seconds part
     Returns the mouse coordinates if Reset button is hovered, to register a reset if clicked
     """
-    resetBut1 = pygame.Rect(162, 26, 132, 44) # Reset buttom for level EASY
-    resetBut2 = pygame.Rect(333, 30, 132, 44) # Reset buttom for levels MEDIUM AND HARD
+    resetBut1 = pygame.Rect(162, 26, 132, 44)  # Reset buttom for level EASY
+    resetBut2 = pygame.Rect(333, 30, 132, 44)  # Reset buttom for levels MEDIUM AND HARD
     resetHovered = False
     mouse_pos = pygame.mouse.get_pos()
     if (SCREEN_WIDTH, SCREEN_HEIGHT) == (322, 410):
@@ -114,25 +116,26 @@ def score_space(startTime):
                            font_type='Consolas')
     else:
         WIN.blit(scoreSpaceRect2, (PADDING, PADDING, 771, 80))
-        text_to_screen(WIN, PADDING + 40, PADDING + 31, 'FLAGS:', SIZE=20, color=(242,242,242))
-        text_to_screen(WIN, PADDING + 650, PADDING + 31, 'TIME:', SIZE=20, color=(242,242,242))
-        text_to_screen(WIN, PADDING + 120, PADDING + 32, TOTAL_FLAGS, SIZE=20, color=(242,242,242),
+        text_to_screen(WIN, PADDING + 40, PADDING + 31, 'FLAGS:', SIZE=20, color=(242, 242, 242))
+        text_to_screen(WIN, PADDING + 650, PADDING + 31, 'TIME:', SIZE=20, color=(242, 242, 242))
+        text_to_screen(WIN, PADDING + 120, PADDING + 32, TOTAL_FLAGS, SIZE=20, color=(242, 242, 242),
                        font_type='Consolas')
         WIN.blit(reset, (333, 27))
         if resetBut2.collidepoint(mouse_pos):
             WIN.blit(resetHover, (333, 27))
             resetHovered = True
         if startTime == 0:
-            text_to_screen(WIN, PADDING + 714, PADDING + 32, startTime, SIZE=20, color=(242,242,242),
+            text_to_screen(WIN, PADDING + 714, PADDING + 32, startTime, SIZE=20, color=(242, 242, 242),
                            font_type='Consolas')
         else:
             seconds = (pygame.time.get_ticks() - startTime) // 1000
-            text_to_screen(WIN, PADDING + 714, PADDING + 32, seconds, SIZE=20, color=(242,242,242),
+            text_to_screen(WIN, PADDING + 714, PADDING + 32, seconds, SIZE=20, color=(242, 242, 242),
                            font_type='Consolas')
     if resetHovered:
         return mouse_pos
     else:
         return None
+
 
 def end_animation(message, timeFreeze):
     """
@@ -166,7 +169,7 @@ def end_animation(message, timeFreeze):
             if restartBut1.collidepoint(mouse_pos):
                 WIN.blit(restartHover, (100, 27))
             else:
-                WIN.blit(restart, (100,27))
+                WIN.blit(restart, (100, 27))
             if message == 'YOU LOSE':
                 WIN.blit(youLose, (78, 120))
             else:
@@ -197,18 +200,19 @@ def tile_clicked(mouseBut):
     or calls for checking the neighbour checking
     """
     coords = find_clicked_coords()
-    if coords != None:
+    if coords is not None:
         (x, y) = coords
         if mouseBut == 0:
-            if GRID[x][y]['bomb'] == True:
+            if GRID[x][y]['bomb']:
                 return 'Bomb'
             else:
-                total_clicked_tiles = count_neighbours(coords)
+                count_neighbours(coords)
         else:
-            if GRID[x][y]['flag'] == False:
-                 GRID[x][y]['flag'] = True
+            if not GRID[x][y]['flag']:
+                GRID[x][y]['flag'] = True
             else:
                 GRID[x][y]['flag'] = False
+
 
 def initalize_tiles():
     """
@@ -219,16 +223,17 @@ def initalize_tiles():
     for x in range(SIZE[1]):
         for y in range(SIZE[0]):
             GRID[x][y] = {
-                            'state': 'notClicked',
-                            'BOMBSAround': 0,
-                            'flag': False,
-                            'bomb': False,
-                            'rect': pygame.Rect(PADDING + y*TILESIZE + y*GAP, PADDING + x*TILESIZE
-                                        + x*GAP + 88, TILESIZE, TILESIZE),
-                            'coords': (PADDING + y*TILESIZE + y*GAP,
-                                        PADDING + x*TILESIZE + x*GAP + 88)
-                            }
+                'state': 'notClicked',
+                'BOMBSAround': 0,
+                'flag': False,
+                'bomb': False,
+                'rect': pygame.Rect(PADDING + y * TILESIZE + y * GAP, PADDING + x * TILESIZE
+                                    + x * GAP + 88, TILESIZE, TILESIZE),
+                'coords': (PADDING + y * TILESIZE + y * GAP,
+                           PADDING + x * TILESIZE + x * GAP + 88)
+            }
     return GRID
+
 
 def display_tiles():
     """
@@ -245,55 +250,59 @@ def display_tiles():
             else:
                 WIN.blit(grayTile, GRID[x][y]['rect'])
             if GRID[x][y]['BOMBSAround'] > 0 and GRID[x][y]['bomb'] == False:
-                text_to_screen(WIN, GRID[x][y]['coords'][0]+TILESIZE*(2/5),
-                               GRID[x][y]['coords'][1]+TILESIZE*(2/7), GRID[x][y]['BOMBSAround'],
+                text_to_screen(WIN, GRID[x][y]['coords'][0] + TILESIZE * (2 / 5),
+                               GRID[x][y]['coords'][1] + TILESIZE * (2 / 7), GRID[x][y]['BOMBSAround'],
                                SIZE=14)
-            if GRID[x][y]['flag'] == True:
+            if GRID[x][y]['flag']:
                 if GRID[x][y]['state'] == 'notClicked':
                     TOTAL_FLAGS -= 1
-                    newFlagPosition = ((GRID[x][y]['coords'][0]+(2/5)*TILESIZE),
-                                       GRID[x][y]['coords'][1]+(1/7)*TILESIZE)
+                    newFlagPosition = ((GRID[x][y]['coords'][0] + (2 / 5) * TILESIZE),
+                                       GRID[x][y]['coords'][1] + (1 / 7) * TILESIZE)
                     WIN.blit(flag, newFlagPosition)
 
             if GRID[x][y]['state'] == 'clicked':
                 clicked_tile_count += 1
-    if clicked_tile_count == (SIZE[0]*SIZE[1] - BOMB):
+    if clicked_tile_count == (SIZE[0] * SIZE[1] - BOMB):
         return True
+
 
 def bomb_gen(pos):
     """
     Used to generate the BOMBS randomly. This is called when the first clicks is called.
     Takes position of the click and return the bomb list.
     """
-    (x,y) = find_clicked_coords()
+    (x, y) = find_clicked_coords()
     BOMBSGenerated = 0
     bomb = []
-    tiles3x3 = [(x,y), (x-1,y), (x+1,y), (x, y-1), (x, y+1), (x-1, y-1), (x+1, y+1), (x-1, y+1), (x+1, y-1)]
+    tiles3x3 = [(x, y), (x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1), (x - 1, y - 1), (x + 1, y + 1), (x - 1, y + 1),
+                (x + 1, y - 1)]
     for index in range(len(tiles3x3)):
         if -1 < tiles3x3[index][0] < SIZE[1] and -1 < tiles3x3[index][1] < SIZE[0]:
-            tiles3x3[index] = tiles3x3[index][0]*SIZE[0] + tiles3x3[index][1]
+            tiles3x3[index] = tiles3x3[index][0] * SIZE[0] + tiles3x3[index][1]
     while BOMBSGenerated < BOMB:
-        randomBomb = random.randint(0,SIZE[0]*SIZE[1])
-        if not(randomBomb in tiles3x3 or randomBomb in bomb):
+        randomBomb = random.randint(0, SIZE[0] * SIZE[1])
+        if not (randomBomb in tiles3x3 or randomBomb in bomb):
             bomb.append(randomBomb)
             BOMBSGenerated += 1
     for x in range(SIZE[1]):
         for y in range(SIZE[0]):
-            if (x*SIZE[0]+y) in bomb:
+            if (x * SIZE[0] + y) in bomb:
                 GRID[x][y]['bomb'] = True
     return bomb
+
 
 def mouse_hover():
     """
     Returns True when a valid tile is hovered
     """
     coords = find_clicked_coords()
-    if coords != None:
+    if coords is not None:
         x, y = coords[0], coords[1]
         if GRID[x][y]['state'] == 'notClicked':
-            if GRID[x][y]['flag'] == False:
+            if not GRID[x][y]['flag']:
                 WIN.blit(grayTile, GRID[x][y]['rect'])
             return True
+
 
 def find_clicked_coords():
     """
@@ -302,12 +311,13 @@ def find_clicked_coords():
     pos = pygame.mouse.get_pos()
     coords = None
     i, j = pos
-    i = ((i-8) // 31)
-    j = ((j-96) // 31)
+    i = ((i - 8) // 31)
+    j = ((j - 96) // 31)
     if 0 <= i < SIZE[0] and 0 <= j < SIZE[1]:
         if GRID[j][i]['rect'].collidepoint(pos):
             coords = (j, i)
     return coords
+
 
 def count_neighbours(coords):
     """
@@ -316,28 +326,27 @@ def count_neighbours(coords):
     Both are used just for the heck of it. Has minimal effect on performance, but probably
     significant effect on readability. May change in the future.
     """
-    flag = False
-    (x,y) = coords
-    queue1 = Queue(maxsize=SIZE[0]*SIZE[1])
-    countBomb = 0
+    (x, y) = coords
+    queue1 = Queue(maxsize=SIZE[0] * SIZE[1])
     GRID[x][y]['state'] = 'clicked'
+
     def tile_reveal(coords):
         """
         This fun() is the one called recursively.
         """
-        (x,y) = coords
+        (x, y) = coords
         countBomb = 0
-        queue2 = Queue(maxsize=SIZE[0]*SIZE[1])
-        for i in range(-1, 2): # this nested for loops is used to check the 8 tiles around it
-            for j in range(-1,2):
-                (x1,y1) = (x+i, y+j)
-                if (-1 < y1 < SIZE[0]) and (-1 < x1 < SIZE[1]) and (i,j) != 0:
-                    if GRID[x1][y1]['bomb'] == False:
+        queue2 = Queue(maxsize=SIZE[0] * SIZE[1])
+        for i in range(-1, 2):  # this nested for loops is used to check the 8 tiles around it
+            for j in range(-1, 2):
+                (x1, y1) = (x + i, y + j)
+                if (-1 < y1 < SIZE[0]) and (-1 < x1 < SIZE[1]) and (i, j) != 0:
+                    if not GRID[x1][y1]['bomb']:
                         if GRID[x1][y1]['state'] == 'notClicked':
                             queue2.put((x1, y1))
                     else:
                         countBomb += 1
-        if countBomb == 0: # if 8 surrounding BOMBSa are not BOMBS they are pushed into the main queue
+        if countBomb == 0:  # if 8 surrounding BOMBSa are not BOMBS they are pushed into the main queue
             while not queue2.empty():
                 i = queue2.get(0)
                 GRID[i[0]][i[1]]['state'] = 'clicked'
@@ -350,8 +359,10 @@ def count_neighbours(coords):
             tile_reveal(queue1.get(0))
         else:
             return None
+
     tile_reveal(coords)
     pygame.time.wait(40)
+
 
 def main_menu():
     """
@@ -360,21 +371,23 @@ def main_menu():
     global SCREEN_WIDTH, SCREEN_HEIGHT
     (SCREEN_WIDTH, SCREEN_HEIGHT) = (322, 410)
     WIN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE)
+    if sys.platform in ["WIN32", "WIN64"]:
+        os.environ["SDL_VIDEO_CENTERED"] = "1"
     WIN.fill((146, 146, 146))
     levels = {
-        'easy' : {
-            'windowsize' : (322, 410),
-            'gridsize' : (10, 10)
+        'easy': {
+            'windowsize': (322, 410),
+            'gridsize': (10, 10)
         },
 
-        'medium' : {
-            'windowsize' : (787, 410),
-            'gridsize' : (25, 10)
+        'medium': {
+            'windowsize': (787, 410),
+            'gridsize': (25, 10)
         },
 
-        'hard' : {
-            'windowsize' : (787, 534),
-            'gridsize'  : (25, 14)
+        'hard': {
+            'windowsize': (787, 534),
+            'gridsize': (25, 14)
         }
 
     }
@@ -383,6 +396,7 @@ def main_menu():
     hardRect = hard.get_rect().move((80, 235))
     menuSelection = None
     while True:
+        WIN.fill((146, 146, 146))
         mouse_pos = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -390,11 +404,11 @@ def main_menu():
                 sys.exit()
             elif event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0] == 1:
                 if easyRect.collidepoint(mouse_pos):
-                    menuSelection = (levels['easy']['windowsize'], levels['easy']['gridsize'],20,WIN)
+                    menuSelection = (levels['easy']['windowsize'], levels['easy']['gridsize'], 20, WIN)
                 elif mediumRect.collidepoint(mouse_pos):
-                    menuSelection = (levels['medium']['windowsize'], levels['medium']['gridsize'],50,WIN)
+                    menuSelection = (levels['medium']['windowsize'], levels['medium']['gridsize'], 50, WIN)
                 elif hardRect.collidepoint(mouse_pos):
-                    menuSelection = (levels['hard']['windowsize'], levels['hard']['gridsize'],70,WIN)
+                    menuSelection = (levels['hard']['windowsize'], levels['hard']['gridsize'], 70, WIN)
         WIN.blit(easy, (80, 93))
         if easyRect.collidepoint(mouse_pos):
             WIN.blit(easyHover, (80, 93))
@@ -404,13 +418,14 @@ def main_menu():
         WIN.blit(hard, (80, 235))
         if hardRect.collidepoint(mouse_pos):
             WIN.blit(hardHover, (80, 235))
-        if menuSelection != None:
+        if menuSelection is not None:
             return menuSelection
         pygame.display.update()
         FPSCLOCK.tick(30)
         pygame.time.wait(30)
 
-def text_to_screen(WIN, x, y, n, font_type='Lucida Console', color=(242,242,242),SIZE=int(TILESIZE *(3/5))):
+
+def text_to_screen(WIN, x, y, n, font_type='Lucida Console', color=(242, 242, 242), SIZE=int(TILESIZE * (3 / 5))):
     """
     This fun() displays the fonts, takes in the detail of it and displays it accordingly.
     x,y is the coordinates, and n is the text.
@@ -420,6 +435,7 @@ def text_to_screen(WIN, x, y, n, font_type='Lucida Console', color=(242,242,242)
     text = font.render(text, True, color)
     WIN.blit(text, (x, y))
 
+
 def display_bomb():
     """
     Used to blit the mines or BOMBS
@@ -427,8 +443,9 @@ def display_bomb():
     for x in range(SIZE[1]):
         for y in range(SIZE[0]):
             WIN.blit(grayTile, GRID[x][y]['coords'])
-            if (x*SIZE[0]+y) in BOMBS:
+            if (x * SIZE[0] + y) in BOMBS:
                 WIN.blit(mine, (GRID[x][y]['coords']))
+
 
 menuSelection = main_menu()
 main()
